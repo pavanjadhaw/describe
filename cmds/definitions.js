6272a76b1f247cb73874d0aa95be4c4eb81095c7
Pavan Jadhaw <pavanjadhaw96@gmail.com>
2018-10-26 20:39:37 +0530

    update: now get definitions for multiple words

--- cmds/definitions.js
+++ cmds/definitions.js
@@ -3,20 +3,22 @@ const getWord = require('../utils/query');
 const handleError = require('../utils/error');
 
 module.exports = async args => {
-  const spinner = ora().start();
+  for (let i = 0; i < args._.length; i++) {
+    const spinner = ora().start();
 
-  try {
-    const path = '/entries/en/' + args._[0].toLowerCase();
-    const result = await getWord(path);
+    try {
+      const path = '/entries/en/' + args._[i].toLowerCase();
+      const result = await getWord(path);
 
-    spinner.stop();
+      spinner.stop();
 
-    const definition =
-      result.lexicalEntries[0].entries[0].senses[0].definitions[0];
+      const definition =
+        result.lexicalEntries[0].entries[0].senses[0].definitions[0];
 
-    console.log(`${args._[0]} is ${definition}`);
-  } catch (error) {
-    spinner.stop();
-    handleError(error.response.status);
+      console.log(`${args._[i]} is ${definition}`);
+    } catch (error) {
+      spinner.stop();
+      handleError(error.response.status);
+    }
   }
 };
